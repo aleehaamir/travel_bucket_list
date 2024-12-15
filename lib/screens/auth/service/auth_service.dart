c5a7390b79498a8a8aceb54eb213e00c1c373be6
Khizar Aslam <khizaraslam201@gmail.com>
2024-12-16 00:50:10 +0500

    auth fixed

--- lib/screens/auth/service/auth_service.dart
+++ lib/screens/auth/service/auth_service.dart
@@ -12,37 +12,43 @@ mixin AuthService {
     required String email,
     required String password,
   }) async {
-    // List<UserModel> saveUsers = [
-    //   UserModel(name: "usama", email: 'email', password: 'pass'),
-    //   UserModel(name: "usama1", email: 'email1', password: 'pass1'),
-    //   UserModel(name: "usama2", email: 'email2', password: 'pass2'),
-    // ];
-    // await HiveService().addBoxAndDelete(saveUsers, HiveService().usersList);
-    // await HiveService().addBoxAndDelete(<UserModel>[], HiveService().usersList);
-    List<dynamic> getUsers =
-        await HiveService().getBoxes(HiveService().usersList);
-    List<dynamic> users = getUsers.first;
-    if (users.any((element) => element.name == name)) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("User name $name already exist")));
-      return;
-    } else if (users.any((element) => element.email == email)) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("User email $email already exist")));
-      return;
+    if (await HiveService().isExists(boxName: HiveService().usersList)) {
+      List<dynamic> getUsers =
+          await HiveService().getBoxes(HiveService().usersList);
+      List<dynamic> users = getUsers.first;
+      if (users.any((element) => element.name == name)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("User name $name already exist")));
+        return;
+      } else if (users.any((element) => element.email == email)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("User email $email already exist")));
+        return;
+      }
+      users.add(UserModel(name: name, email: email, password: password));
+
+      await HiveService().addBoxAndDelete(users, HiveService().usersList);
+
+      await HiveService().addBoxAndDelete(
+          UserModel(name: name, email: email, password: password),
+          HiveService().currentUser);
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    } else {
+      await HiveService().addBoxAndDelete(
+          [UserModel(name: name, email: email, password: password)],
+          HiveService().usersList);
+      await HiveService().addBoxAndDelete(
+          UserModel(name: name, email: email, password: password),
+          HiveService().currentUser);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
     }
-    users.add(UserModel(name: name, email: email, password: password));
-
-    await HiveService().addBoxAndDelete(users, HiveService().usersList);
-
-    await HiveService().addBoxAndDelete(
-        UserModel(name: name, email: email, password: password),
-        HiveService().currentUser);
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const HomeScreen()),
-    );
   }
 
   void login({
@@ -50,24 +56,30 @@ mixin AuthService {
     required String name,
     required String password,
   }) async {
-    List<dynamic> getUsers =
-        await HiveService().getBoxes(HiveService().usersList);
-    List<dynamic> users = getUsers.first;
-    if (users.any(
-        (element) => element.name == name && element.password == password)) {
-      int index = users.indexWhere(
-          (element) => element.name == name && element.password == password);
-      await HiveService().addBoxAndDelete(
-          UserModel(
-              name: users[index].name,
-              email: users[index].email,
-              password: users[index].password),
-          HiveService().currentUser);
+    if (await HiveService().isExists(boxName: HiveService().usersList)) {
+      List<dynamic> getUsers =
+          await HiveService().getBoxes(HiveService().usersList);
+      List<dynamic> users = getUsers.first;
+      if (users.any(
+          (element) => element.name == name && element.password == password)) {
+        int index = users.indexWhere(
+            (element) => element.name == name && element.password == password);
+        await HiveService().addBoxAndDelete(
+            UserModel(
+                name: users[index].name,
+                email: users[index].email,
+                password: users[index].password),
+            HiveService().currentUser);
 
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Invalid username or password")));
+        return;
+      }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text("Invalid username or password")));
